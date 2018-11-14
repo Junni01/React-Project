9e721066d831f424d2e70f6f78a4b47039654028
Saku Junni <junnisaku@gmail.com>
2018-11-14 14:42:30 +0200

    Initial Commist

---
+++ ReactAppV2/wwwroot/js/Tutorial.jsx
@@ -0,0 +1,22 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+
+        );
+    }
+}
+
+class InnerBox extends React.Component {
+  render() {
+    return (
+      <button>Moi</button>
+    );
+
+  }
+}
+
+
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
+ReactDOM.render(<InnerBox />, document.getElementById('inside-content'));
